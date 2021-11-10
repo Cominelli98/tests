ebcfcb4de56a2fa251b3a198f901cc5a9939361d
Cominelli98 <a.cominelli009@studenti.unibs.it>
2021-11-10 10:50:45 +0100

    finita momentaneamente UtilityTest

--- src/tests/SimulatoreTest.java
+++ src/tests/SimulatoreTest.java
@@ -1,15 +1,23 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import it.unibs.ingesw.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimulatoreTest {
 
-	@Before
+	Simulatore simulatore;
+	
+	@BeforeEach
 	public void setUp() {
+		
 		Network net1 = new Network("Net");
 		net1.addTransition("tran");
 		net1.addLocation("loc1");
@@ -20,46 +28,16 @@ class SimulatoreTest {
 		net1.addLink(l2);
 		
 		Petri_network pnet = new Petri_network(net1, "PNet");
-	}
-	
-	@After
-	public void tearDown() {
-		
+		simulatore = new Simulatore(pnet);
 	}
 	
 	@Test
-	public void AttivabileTest() {
-	
-//		ArrayList<Network> netList = new ArrayList<Network>();
-//		Menu_Reti.createNetwork(netList);
-		Network net1 = new Network("Paganel");
-		Network net2 = new Network("Paganela");
-//		netList.add(net1);
-//		netList.add(net2);
-		net1.addLocation("loc1-0");
-		net2.addLocation("loc2-0");
-		net1.addLocation("loc1-1");
-		net2.addLocation("loc2-1");
-		net1.addTransition("tran1");
-		net2.addTransition("tran2");
-		Link l10 = new Link(net1.getLocation(0).getId(), net1.getTransition(0).getId(), net1.getId(), 1);
-		Link l11 = new Link(net1.getTransition(0).getId(), net1.getLocation(1).getId(), net1.getId(), 1);
-		Link l20 = new Link(net2.getLocation(0).getId(), net2.getTransition(0).getId(), net2.getId(), 1);
-		Link l21 = new Link(net2.getTransition(0).getId(), net2.getLocation(1).getId(), net2.getId(), 1);
+	public void nextStepTest() {
+		
+		
 
 		
-		
-//		AssertEquals(net1, net2);
-		assertNotEquals(net1.getName(), net2.getName());
-//		assertEquals(net1.getLocations().size(), net2.getLocations().size());
-//		assertEquals(net1.getName(), net2.getName(), "ciao");
-		assertAll(
-				() -> assertEquals(net1.getLocations().size(), net2.getLocations().size()),
-				() -> assertEquals(net1.getTransitions().size(), net2.getTransitions().size()),
-				() -> assertEquals(l10.getDestination(), l11.getOrigin())
-				);
-//		
-		
 	}
+	
 
 }
--- src/tests/UtilityTest.java
+++ src/tests/UtilityTest.java
@@ -2,41 +2,70 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
+import org.junit.jupiter.api.Test;
+import it.unibs.ingesw.Network;
 import it.unibs.ingesw.utility.Utility;
 
 class UtilityTest {
 
-	@Before
-	public void setUp() {
-		
-	}
-	
 	@Test
-	public void testReadLimitedInt() {
+	void testReadLimitedInt() {
 		
 		System.out.println("Test lettura interi limitati sup e inf");
+		System.out.println("inserisci il min: ");
 		var min = Utility.readInt();
+		System.out.println("inserisci il max: ");
 		var max = Utility.readInt();
 		assertTrue(Utility.readLimitedInt(min, max) <= max 
 					|| Utility.readLimitedInt(min, max) >= min);
 	}
 	
 	@Test
-	public void testReadLowLimitInt() {
+	void testReadLowLimitInt() {
 		
 		System.out.println("Test lettura interi limitati inf");
+		System.out.println("inserisci il min: ");
 		var min = Utility.readInt();
 		assertTrue(Utility.readLowLimitInt(min) >= min);
 	}
+	
 	@Test
 	public void testReadString() {
 		
 		System.out.println("Test lettura Stringhe");
 		var s = "hello";
+		System.out.println("say hello: ");
 		assertTrue(Utility.readString().equals(s));
+	}
+	
+	@Test
+	void nameCheckTest() {
+		
+		System.out.println("Test nameCheck");
+		String toTest = "Banana Split";
+		Network net1 = new Network("Banana Split");
+		net1.addLocation("loc1-0");
+		net1.addTransition("tran1");
+		
+		assertTrue(Utility.nameCheck(net1, toTest));
+		assertFalse(Utility.nameCheck(net1.getLastLocation(), toTest));
+		assertFalse(Utility.nameCheck(net1.getLastTransition(), toTest));
+	}
+	
+	@Test
+	void getMaxTest() {
+		
+		
+		Integer[] c = {1, 5, 10, 50, 20, 30, 98, 100, 2, 30};
+		ArrayList<Integer> ali = new ArrayList<>();
+		for (int i = 0; i < c.length; i++) {
+			ali.add(c[i]);
+		}
+		
+		assertEquals(Utility.getMax(ali), 100);
+		assertNotEquals(Utility.getMax(ali), 1);
 	}
 
 }
