81723c764a25ce40bcc68d311aabc8e786c86738
Cominelli98 <a.cominelli009@studenti.unibs.it>
2022-01-04 16:52:10 +0100

    fixato utilitytest

--- src/tests/UtilityTest.java
+++ src/tests/UtilityTest.java
@@ -9,49 +9,16 @@ import it.unibs.ingesw.Network;
 import it.unibs.ingesw.utility.Utility;
 
 class UtilityTest {
-
-	@Test
-	void testReadLimitedInt() {
-		
-		System.out.println("Test lettura interi limitati sup e inf");
-		System.out.println("inserisci il min: ");
-		var min = Utility.readInt();
-		System.out.println("inserisci il max: ");
-		var max = Utility.readInt();
-		System.out.println("inserisci il numero di test: ");
-		
-		assertTrue(Utility.readLimitedInt(min, max) <= max 
-					|| Utility.readLimitedInt(min, max) >= min);
-	}
-	
-	@Test
-	void testReadLowLimitInt() {
-		
-		System.out.println("Test lettura interi limitati inf");
-		System.out.println("inserisci il min: ");
-		var min = Utility.readInt();
-		
-		assertTrue(Utility.readLowLimitInt(min) >= min);
-	}
-	
-	@Test
-	public void testReadString() {
-		
-		System.out.println("Test lettura Stringhe");
-		var s = "hello";
-		System.out.println("say hello: ");
-		
-		assertTrue(Utility.readString().equals(s));
-	}
 	
 	@Test
 	void nameCheckTest() {
 		
 		String toTest = "Banana Split";
 		Network net1 = new Network("Banana Split");
-		net1.addLocation("loc1-0");
+		net1.addLocation("loc1");
 		net1.addTransition("tran1");
 		assertAll(
+			"Errore comparazione stringhe",
 			() -> assertTrue(Utility.nameCheck(net1, toTest)),
 			() -> assertFalse(Utility.nameCheck(net1.getLastLocation(), toTest)),
 			() -> assertFalse(Utility.nameCheck(net1.getLastTransition(), toTest))
@@ -61,14 +28,16 @@ class UtilityTest {
 	@Test
 	void getMaxTest() {
 		
-		Integer[] c = {1, 5, 10, 50, 20, 30, 98, 100, 2, 30};
-		ArrayList<Integer> ali = new ArrayList<>();
+		Integer[] c = {-1, -100, 1, 5, 10, 50, 20, 30, 98, 100, 2, 30};
+		ArrayList<Integer> numbs = new ArrayList<>();
 		for (int i = 0; i < c.length; i++) {
-			ali.add(c[i]);
+			numbs.add(c[i]);
 		}
 		assertAll(
-			() -> assertEquals(Utility.getMax(ali), 100),
-			() -> assertNotEquals(Utility.getMax(ali), 1)
+			"Errore lettura del massimo inesatta",
+			() -> assertEquals(Utility.getMax(numbs), 100),
+			() -> assertNotEquals(Utility.getMax(numbs), -100),
+			() -> assertNotEquals(Utility.getMax(numbs), 1)
 		);
 	}
 
